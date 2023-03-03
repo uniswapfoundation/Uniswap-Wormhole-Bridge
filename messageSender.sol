04b98b64b93daf2b8261851a81bd3fb79909ae3a
Jonathan Claudius <jclaudius@jumptrading.com>
2023-03-03 03:06:20 +0000

    Add licence header

--- messageSender.sol
+++ messageSender.sol
@@ -1,3 +1,19 @@
+/**
+ * Copyright Uniswap Foundation 2023
+ * 
+ * This code is based on code deployed here: https://bscscan.com/address/0x3ee84fFaC05E05907E6AC89921f000aE966De001#code 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 pragma solidity ^0.8.9;
 
 interface IWormhole {
