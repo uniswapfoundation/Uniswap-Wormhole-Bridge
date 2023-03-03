f03cdd6c75ec7ca5519c070809efa2b6025c2091
njayakumar <njayakumar@jumptrading.com>
2023-03-03 18:35:16 +0000

    adding tests and bug fix

---
+++ src/interfaces/IUniswapWormholeMessageSender.sol
@@ -0,0 +1,8 @@
+// contracts/Structs.sol
+// SPDX-License-Identifier: Apache 2
+
+pragma solidity ^0.8.9;
+
+interface IUniswapWormholeMessageSender {
+    function sendMessage(address[] memory targets, uint256[] memory values, bytes[] memory datas, address messageReceiver, uint16 receiverChainId) external;
+}
