7bd84dd2ba0cb525354c77018619935f6fbdf6be
Jonathan Claudius <jclaudius@jumptrading.com>
2023-03-03 04:53:36 +0000

    Add sender test stub

---
+++ test/UniswapWormholeMessageSender.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/UniswapWormholeMessageSender.sol";
+
+contract UniswapWormholeMessageSenderTest is Test {
+    /** TODO: build out the test cases for this contract */
+    function testX(uint256 x) public {
+        assertEq(x, x);
+    }
+}
